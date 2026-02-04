3dbba77f5d10669016664f702f0003f5e1ac9e4d
Théo Poizat <hello@zatteo.com>
2026-02-04 16:35:39 +0100

    feat: Add API login methods

---
+++ src/helpers/apiLogin.js
@@ -0,0 +1,33 @@
+export const buildApiLoginUrl = async ({ orgId, domain }) => {
+  return new URL(`https://api-login-${orgId}.${domain}`)
+}
+
+export const fetchAuthStatus = async ({ apiLoginUrl }) => {
+  const url = `${apiLoginUrl.origin}/api/auth/status`
+  const statusResponse = await fetch(url, {
+    credentials: 'include'
+  })
+
+  if (!statusResponse.ok) {
+    throw new Error(`Failed auth/status fetch: ${statusResponse.status}`)
+  }
+
+  const statusData = await statusResponse.json()
+
+  return !!(statusData.authenticated && statusData.homeserver)
+}
+
+export const fetchRedirectLoginUrl = async ({ apiLoginUrl }) => {
+  const url = `${apiLoginUrl.origin}/api/auth/login-url`
+  const loginUrlResponse = await fetch(url, {
+    credentials: 'include'
+  })
+
+  if (!loginUrlResponse.ok) {
+    throw new Error(`Failed auth/login-url fetch: ${loginUrlResponse.status}`)
+  }
+
+  const loginData = await loginUrlResponse.json()
+
+  return loginData.loginUrl
+}
