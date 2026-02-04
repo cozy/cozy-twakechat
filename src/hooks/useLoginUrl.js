b164db6b23b0e189222b1c63ded0b1fe45330517
Théo Poizat <hello@zatteo.com>
2026-02-04 16:40:18 +0100

    feat: Use new login flow

---
+++ src/hooks/useLoginUrl.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+
+import { useClient } from 'cozy-client'
+import Minilog from 'cozy-minilog'
+
+import {
+  buildApiLoginUrl,
+  fetchAuthStatus,
+  fetchRedirectLoginUrl
+} from '@/helpers/apiLogin'
+import {
+  getFreshLoginParams,
+  isFreshLogin,
+  cleanFreshLoginUrl,
+  buildFreshLoginUrl
+} from '@/helpers/freshLogin'
+import { fetchInstanceOrgId, getInstanceDomain } from '@/helpers/instanceData'
+
+const log = Minilog('👨‍💻 [Login wrapper]')
+
+const getLoginUrl = async ({ appUrl, apiLoginUrl }) => {
+  const freshLoginParams = getFreshLoginParams()
+  log.info('Fresh login params', freshLoginParams)
+
+  if (freshLoginParams.error) {
+    throw new Error(
+      `Error in fresh login params: ${freshLoginParams.error} ${freshLoginParams.errorDescription}`
+    )
+  }
+
+  if (isFreshLogin({ freshLoginParams })) {
+    log.info('🔵 Fresh login')
+    cleanFreshLoginUrl()
+    return buildFreshLoginUrl({ freshLoginParams, appUrl })
+  } else {
+    log.info('🔵 Not fresh login')
+    const isAuthenticated = await fetchAuthStatus({ apiLoginUrl })
+    if (isAuthenticated) {
+      log.info('🔵 Authenticated')
+      // TODO: not correct URL? Need to add orgId ?
+      return appUrl.toString()
+    } else {
+      log.info('🔵 Not authenticated')
+      window.location.href = await fetchRedirectLoginUrl({ apiLoginUrl })
+    }
+  }
+}
+
+export const useLoginUrl = embeddedAppUrl => {
+  const client = useClient()
+  const [loginUrl, setLoginUrl] = useState()
+
+  useEffect(() => {
+    async function asyncGetLoginUrl() {
+      const orgId = await fetchInstanceOrgId(client)
+      log.info('Org ID:', orgId)
+
+      const domain = await getInstanceDomain(client)
+      log.info('Domain:', domain)
+
+      const appUrl = new URL(embeddedAppUrl)
+      log.info('App URL:', appUrl.toString())
+
+      const apiLoginUrl = await buildApiLoginUrl({ orgId, domain })
+      log.info('API login URL:', apiLoginUrl.toString())
+
+      const url = await getLoginUrl({ appUrl, apiLoginUrl })
+
+      setLoginUrl(url)
+    }
+    asyncGetLoginUrl()
+  }, [])
+
+  log.info('URL to load', loginUrl)
+
+  return loginUrl
+}
