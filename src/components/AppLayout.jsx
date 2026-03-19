0eef8fe26719b6434399677f79ce0ae78d61aa71
Théo Poizat <hello@zatteo.com>
2026-03-19 11:03:02 +0100

    feat: update cozy-bar to 30.0.0 and cozy-ui to 137.0.0

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -3,10 +3,17 @@ import { Outlet } from 'react-router-dom'
 
 import { BarComponent } from 'cozy-bar'
 
+import Chat from './Icons/Chat'
+import ChatText from './Icons/ChatText'
+
 const AppLayout = () => {
   return (
     <>
-      <BarComponent searchOptions={{ enabled: false }} />
+      <BarComponent
+        searchOptions={{ enabled: false }}
+        appIcon={Chat}
+        appTextIcon={ChatText}
+      />
       <Outlet />
     </>
   )
